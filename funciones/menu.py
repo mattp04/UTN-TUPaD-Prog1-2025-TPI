fc4e31614c9c01fdeab93b36d87be915909b52fb
lucasdperez96 <lucasdperez.96@gmail.com>
2025-09-29 12:10:57 -0300

    Update menu.py
    division de trabajo

--- funciones/menu.py
+++ funciones/menu.py
@@ -1,15 +1,15 @@
 # 2) Funcionalidades mínimas del sistema
 # El programa debe ofrecer un menú de opciones en consola que permita:
-# • Buscar un país por nombre (coincidencia parcial o exacta).
-# • Filtrar países por:
+# • Buscar un país por nombre (coincidencia parcial o exacta). (Lucas)
+# • Filtrar países por:(Mati)
 # o Continente
 # o Rango de población
 # o Rango de superficie
-# • Ordenar países por:
+# • Ordenar países por: (Lucas)
 # o Nombre
 # o Población
 # o Superficie (ascendente o descendente)
-# • Mostrar estadísticas:
+# • Mostrar estadísticas: (Mati)
 # o País con mayor y menor población
 # o Promedio de población
 # o Promedio de superficie
